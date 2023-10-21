257001aa38aa062aba14c14ba613c87905ec8872
Sahil Chaudhary <sahilch38@gmail.com>
2023-10-21 20:01:42 +0530

    Create isPrime.cpp
    This is a code for returning a number as true or false using the boolean function in an most efficient manner

---
+++ isPrime.cpp
@@ -0,0 +1,37 @@
+//optimised solution for checking a prime
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is not a prime number." << std::endl;
+    }
+
+    return 0;
+}
